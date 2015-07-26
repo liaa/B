271b66d6fcd8850616699a4c4ffc20cf46000050
kidliaa <kidliaa@gmail.com>
2015-07-26 17:11:19 +0800

    css (less) setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var less = require('gulp-less');
 
-gulp.task('browser-sync',function(){
+gulp.task('browser-sync', function () {
 	browserSync.init(null, {
 		server: {
 			baseDir: './'
 		}
 	})
 
-	gulp.watch('./*.html',['bs-reload']);
+	gulp.watch('./*.html', ['bs-reload']);
+	gulp.watch(['./dist/**/*'], ['bs-reload']);
 });
 
 // Function to call for reloading browsers
 gulp.task('bs-reload', function () {
-    browserSync.reload();
+	browserSync.reload();
 });
 
-gulp.task('default', ['browser-sync']);
+
+gulp.task('less', function () {
+	return gulp.src('./src/less/style.less')
+		.pipe(less())
+		.pipe(gulp.dest('./dist/css/'))
+});
+
+gulp.task('default', ['browser-sync','less'],function(){
+	gulp.watch(['src/less/*', 'src/less/**/*'], ['less']);
+});
